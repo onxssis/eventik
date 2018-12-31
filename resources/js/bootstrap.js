6078ab07557435dfb1d2db6a5d8f6ef5c7c1e8c4
Onassis <onxssis@gmail.com>
2018-12-31 12:14:52 +0100

    add bulma and font-awesome

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,3 @@
-
 window._ = require('lodash');
 
 /**
@@ -8,10 +7,7 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
 } catch (e) {}
 
 /**
@@ -35,7 +31,9 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    console.error(
+        'CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token'
+    );
 }
 
 /**
---
+++ resources/views/home/partials/topbar.blade.php
@@ -0,0 +1,11 @@
+<nav role="navigation" aria-label="main navigation">
+    <div class="topbar is-flex-widescreen">
+        <div class="phone">
+            (617)-555-5555
+        </div>
+
+        <div class="contact">contact@btrealestate.co</div>
+
+        <div class="social">social</div>
+    </div>
+</nav>
