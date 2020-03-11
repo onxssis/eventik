6a6356e1acc67fb5337694e469d7526edeee584b
onassis <onxssis@gmail.com>
2020-03-11 06:40:22 +0100

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Repositories\Event\EloquentEventRepository;
 use App\Repositories\Event\IEventRepository;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (app()->environment() === 'production') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
